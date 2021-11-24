3c24d7d42c05fff6afdbdde8adc6040ad5efaa2f
Rauf-pro <87373669+Rauf-pro@users.noreply.github.com>
2021-11-24 14:03:02 +0400

    added files

---
+++ RaufHomework23.11/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RaufHomework23._11.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
